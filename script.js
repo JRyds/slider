70a70e8dc18d80aea13df78cbbaed60142b9b7e3
JRyds <jpryding@gmail.com>
2023-02-17 22:07:12 +0100

    inital update

---
+++ script.js
@@ -0,0 +1,14 @@
+let arrayOfImages = [];
+
+arrayOfImages.push(["images/1.jpg", "Sandman #1", "January 1989"]);
+arrayOfImages.push(["images/2.jpg", "Sandman #2", "February 1989"]);
+arrayOfImages.push(["images/3.jpg", "Sandman #3", "March 1989"]);
+arrayOfImages.push(["images/4.jpg", "Sandman #4", "April 1989"]);
+arrayOfImages.push(["images/5.jpg", "Sandman #5", "May 1989"]);
+arrayOfImages.push(["images/6.jpg", "Sandman #6", "June 1989"]);
+arrayOfImages.push(["images/7.jpg", "Sandman #7", "July 1989"]);
+arrayOfImages.push(["images/8.jpg", "Sandman #8", "August 1989"]);
+arrayOfImages.push(["images/9.jpg", "Sandman #9", "September 1989"]);
+arrayOfImages.push(["images/10.jpg", "Sandman #10", "November 1989"]);
+arrayOfImages.push(["images/11.jpg", "Sandman #11", "December 1989"]);
+arrayOfImages.push(["images/12.jpg", "Sandman #12", "January 1990"]);
